49c90b5ca771d995e743b44c92f8744439ba5420
ashikaslam1111 <111225061+ashikaslam1111@users.noreply.github.com>
2023-03-24 11:24:09 +0600

    Initial commit

---
+++ 1.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int vertex;
+    cin>>vertex;
+    int adja_matrix[vertex][vertex];
+    vector<int>adja_list[vertex];
+    for(int i=0; i<vertex; i++)
+    {
+        for(int j=0; j<vertex; j++)
+        {
+            adja_matrix[i][j]=0;
+            cin>>adja_matrix[i][j];
+            if(adja_matrix[i][j]!=0)
+                adja_list[i].push_back(j);
+        }
+    }
+    cout<<"\n";
+    for(int i=0; i<vertex; i++)
+    {
+        cout<<i<<": ";
+        for(int j:adja_list[i])
+        {
+            cout<<j<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
